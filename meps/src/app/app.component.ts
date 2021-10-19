141573ce13089525808e544f3f6e26d9c18b8e49
DontuL <dontu.lilian@iisgalvanimi.edu.it>
2021-10-19 11:55:44 +0000

    finito

--- meps/src/app/app.component.ts
+++ meps/src/app/app.component.ts
@@ -11,6 +11,7 @@ import { GEOJSON, GeoFeatureCollection } from './models/geojson.model';
 export class AppComponent implements AfterViewInit{
   title = 'server mappe';
   //Aggiungiamo latitudine e longitudine di un luogo
+  
   center: google.maps.LatLngLiteral = { lat: 45.506738, lng: 9.190766 };
   geoJsonObject : GeoFeatureCollection;
   fillColor: string = "#FF0000";  //Colore delle zone catastali
@@ -23,7 +24,8 @@ export class AppComponent implements AfterViewInit{
     //Questi dati dovremmo scaricarli dal server, per ora li abbiamo copiati nel file     gojson.model.ts
     this.geoJsonObject = GEOJSON;  
     console.log(this.geoJsonObject); //stampo l'oggetto geoJsonObject sulla console
-  
+    this.markerGenerator();
+    
   } 
   
   @ViewChild('mapRef') mapRef: GoogleMap;
@@ -31,7 +33,38 @@ export class AppComponent implements AfterViewInit{
     this.mapRef.data.addGeoJson(this.geoJsonObject);
     this.mapRef.data.setStyle(this.styleFunc)
   }
+  styleFunc = (feature:any) =>{
+    console.log(feature.i.id)
+    let newColor = "#FF0000"; //RED
+    if(feature.i.id == 0) newColor = "#00FF00"; //GREEN
+    else newColor = "#0000FF"; //BLUE
+    return ({
+      clickable: false,
+      fillColor: newColor,
+      strokeWeight: 1
+    });
+  }
+  markerGenerator()
+  {
+    this.markerList =[
+      {
+        position : {
+          lng : this.geoJsonObject.features[0].geometry.coordinates[0][0][0],
+          lat : this.geoJsonObject.features[0].geometry.coordinates[0][0][1]
+        },
+        label: String(this.geoJsonObject.features[0].properties.id),
+      },
+      {
+        position : {
+          lng : this.geoJsonObject.features[1].geometry.coordinates[0][0][0],
+          lat : this.geoJsonObject.features[1].geometry.coordinates[0][0][1]
+        },
+        label: String(this.geoJsonObject.features[1].properties.id),
+      },
+    ]
+
+  }
   
   }
-}
+
 
